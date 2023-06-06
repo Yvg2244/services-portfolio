f36e592852e2ef4a3e12d0a09df018ab67dbfb29
Yvg2244 <yvgupta24@gmail.com>
2023-06-06 11:00:31 +0530

    headline seaction inserted

--- app/page.js
+++ app/page.js
@@ -8,6 +8,14 @@ import {
   IconButton,
 } from "@material-tailwind/react";
 import Image from "next/image";
+import project1 from "../assests/project-devsclash.png";
+import project2 from "../assests/project-redowl.png";
+import icon_frontend from "../assests/icons8-frontend.png";
+import icon_cross from "../assests/icons8-cross-platform.png";
+import icon_ui from "../assests/icons8-ui.png";
+import icon_ux from "../assests/icons8-ux.png";
+import icon_seo from "../assests/icons8-seo.png";
+import backgroung_Image from "../assests/splash.png";
 export default function Home() {
   const [openNav, setOpenNav] = useState(false);
 
@@ -52,7 +60,7 @@ export default function Home() {
     </ul>
   );
   return (
-    <>
+    <div className="bg-black">
       <Navbar className="mx-auto max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4">
         <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
           <Typography
@@ -117,7 +125,35 @@ export default function Home() {
           </div>
         </MobileNav>
       </Navbar>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-    </>
+      <main className="flex min-h-screen flex-col items-center justify-between  bg-primary z-[-1] tracking-wide text-white text-[16px]">
+        <section className="flex flex-col relative top-4 pb-[10rem] mb-[5rem]  items-center justify-center gap-[70px]">
+          <Image
+            alt="background effect"
+            className="z-[10]"
+            src={backgroung_Image}
+            fill
+          />
+          <h1 className="flex flex-col z-[20] text-[46px]  font-bold  ">
+            <span className=" px-2 bg-clip-text text-transparent italic bg-gradient-to-r from-[#F83A3A] from-0% via-[#F13CC7] via-50% to-[#7000FF] to-100%">
+              Let&apos;s bring
+            </span>{" "}
+            <span className="px-2"> UI&apos;s to life.</span>
+          </h1>
+          <p
+            className="px-[10px] z-[20] text-center opacity-60
+        "
+          >
+            Experience Web Design at It&apos;s Finest: Seamless Functionality,
+            Striking Visuals and Unforgettable User Experience
+          </p>
+          <Button
+            sx={{ color: "white" }}
+            className="rounded-lg z-[20] tracking-wider font-bold bg-gradient-to-r from-[#F83A3A] from-0% via-[#F13CC7] via-50% to-[#7000FF] to-100% py-[12px] px-[40px]"
+          >
+            Contact Us
+          </Button>
+        </section>
+      </main>
+    </div>
   );
 }
